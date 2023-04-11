7cb257fa7a722da3b12699c9033504703eb95162
Jeremy <[arkantik.pro@gmail.com](mailto:arkantik.pro@gmail.com)>
2023-04-11 18:48:11 +0200

    Modification of the tailwind config file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,65 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-  },
+    extend: {
+      colors:{
+        primary: "#fff4e2",
+        accent: "#F96140",
+        gryPrimary: "#AE0001",
+        grySecondary: "#740001",
+        gryLight: "#EEBA30",
+        gryDark: "#D3A625",
+        slyPrimary: "#2A623D",
+        slySecondary: "#1A472A",
+        slyLight: "#AAAAAA",
+        slyDark: "#5D5D5D",
+        ravPrimary: "#222F5B",
+        ravSecondary: "#0E1A40",
+        ravLight: "#BEBEBE",
+        ravDark: "#946B2D",
+        hufPrimary: "#F0E75E",
+        hufSecondary: "#EEB939",
+        hufLight: "#726255",
+        hufDark: "#372E29",
+        darkmode: "#363F45",
+        lightmode: "#F5F5F5",
+      },
+
+      fontFamily: {
+        sans: ['"Poppins"', 'sans-serif'],
+      },
+
+      fontSize: {
+        '2xl': ['2rem', {
+            lineHeight: "1.5",
+            letterSpacing: '.05em',
+            fontWeight: '700',
+          }],
+        'xl': ['1.414rem', {
+            lineHeight: '1.5',
+            letterSpacing: '.05em',
+            fontWeight: '600',
+          }],
+        'lg': ['1.25rem', {
+            lineHeight: '1.5',
+            fontWeight: '600',
+          }],
+        'base': ['1rem', {
+            lineHeight: '1.5',
+            fontWeight: '500',
+          }],
+        'sm': ['0.875rem', {
+            lineHeight: '1.5',
+            fontWeight: '500',
+          }],
+        'xs': ['0.75rem', {
+            lineHeight: '1.5',
+            fontWeight: '400',
+          }],
+      },
+    },
   plugins: [],
 }
-
+}
