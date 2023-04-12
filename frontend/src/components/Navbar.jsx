6ad982c21bc34e26114e8fc150d8743eb2c77e23
Jeremy <[arkantik.pro@gmail.com](mailto:arkantik.pro@gmail.com)>
2023-04-12 16:54:44 +0200

    Added mobile menu and logged icons

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable react/button-has-type */
+export default function Navbar() {
+  return (
+    <>
+      <div className="logo-wrapper">
+        <img
+          className="max-w-md"
+          src="./public/icon/logo.svg"
+          alt="Logo"
+          width="55"
+          height="40"
+        />
+      </div>
+      <div className="flex gap-[clamp(0.5rem,2vw,2rem)] bg-dark">
+        <ul className="hidden md:flex items-center gap-[clamp(0.5rem,2vw,2rem)] text-primary text-base">
+          <li>Home</li>
+          <li>Play</li>
+          <li>Inventory</li>
+          <li>Account</li>
+        </ul>
+      </div>
+
+      {/* Mobile menu */}
+      <button className="md:hidden">
+        <img
+          src="./public/icon/mobile-menu.svg"
+          alt="mobile-menu"
+          width="38"
+          height="30"
+        />
+      </button>
+
+      <button>
+        <img
+          className="max-w-md"
+          src="./public/icon/logged.svg"
+          alt="connect"
+          width="20"
+          height="20"
+        />
+      </button>
+    </>
+  );
+}
