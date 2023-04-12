3643e236480d8a955e3a239e90c229b4f8865cbd
Jeremy <[arkantik.pro@gmail.com](mailto:arkantik.pro@gmail.com)>
2023-04-12 19:43:54 +0200

    Added title with card

--- frontend/src/components/LoginCard.jsx
+++ frontend/src/components/LoginCard.jsx
@@ -1,7 +1,9 @@
 export default function LoginCard() {
   return (
-    <div>
-      <p>Test</p>
+    <div className="text-dark bg-[#ececec]/30 backdrop-blur-sm rounded-3xl w-[300px] px-12 py-8">
+      <div className="flex-col space-y-2">
+        <h2 className="flex justify-center text-xl">Login</h2>
+      </div>
     </div>
   );
 }
