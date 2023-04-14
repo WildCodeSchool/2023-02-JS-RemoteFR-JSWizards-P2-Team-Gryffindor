a0c2c63af39e830f92811a2f4b7ba8e7c83192da
Jeremy <[arkantik.pro@gmail.com](mailto:arkantik.pro@gmail.com)>
2023-04-14 20:00:23 +0200

    Fix of Play.jsx

--- frontend/src/pages/Play.jsx
+++ frontend/src/pages/Play.jsx
@@ -1,8 +1,14 @@
-import HousesCards from "../components/HousesCards";
+/* eslint-disable import/no-unresolved */
+import HousesCards from "@components/HousesCards";
 
 export default function Play() {
   return (
-    <section>
+    <section className="flex flex-col items-center space-y-8 ">
+      <h2 className="text-xl">Choose your house</h2>
+      <p className="italic">
+        Select the house you want to be in for your fight, your character will
+        depend of the house you pick
+      </p>
       <HousesCards />
     </section>
   );
