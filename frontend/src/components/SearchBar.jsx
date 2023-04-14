9a0162bd7b28c14e7df830afaacb3859fd209b39
Anthony Gorski <38192276+GorskiAnthony@users.noreply.github.com>
2023-04-14 11:20:26 +0200

    Update SearchBar.jsx

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -6,7 +6,11 @@ function SearchBar() {
         placeholder="Search.."
         className="outline-none py-2 pl-9 pr-4 rounded-md w-80 bg-dark"
       />
-      <img src="./public/icon/Glass.svg" className="pl-2 absolute" alt="Loupe" />
+      <img
+        src="./public/icon/Glass.svg"
+        className="pl-2 absolute"
+        alt="loupe"
+      />
     </div>
   );
 }
