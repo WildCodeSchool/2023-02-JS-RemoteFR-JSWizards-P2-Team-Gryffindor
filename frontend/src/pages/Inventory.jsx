c27d3195e099edf655fd375b9cb34aeed2fe0b23
Jeremy <[arkantik.pro@gmail.com](mailto:arkantik.pro@gmail.com)>
2023-04-14 23:01:01 +0200

    Fixed eslint comment

--- frontend/src/pages/Inventory.jsx
+++ frontend/src/pages/Inventory.jsx
@@ -1,6 +1,5 @@
-/* eslint-disable import/no-unresolved */
-import SearchBar from "@components/SearchBar";
-import FilterBtn from "@components/FilterBtn";
+import SearchBar from "../components/SearchBar";
+import FilterBtn from "../components/FilterBtn";
 
 export default function Inventory() {
   return (
