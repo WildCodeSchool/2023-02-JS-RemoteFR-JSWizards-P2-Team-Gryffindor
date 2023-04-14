1da71f115244b7263efc8a9ada9d1aa743f60d56
Jeremy <[arkantik.pro@gmail.com](mailto:arkantik.pro@gmail.com)>
2023-04-14 19:47:45 +0200

    Fix of Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,16 @@
-import Hero from "../components/Hero";
-import DisplayHouses from "../components/DisplayHouses";
+/* eslint-disable import/no-unresolved */
+import Hero from "@components/Hero";
+import DisplayHouses from "@components/DisplayHouses";
 
 export default function Home() {
   return (
-    <section>
-      <h2 className="text-xl">Home</h2>
+    <section className="flex flex-col items-center space-y-8">
       <Hero />
       <DisplayHouses />
+      <div className="flex flex-col justify-center item-start w-full md:max-w-[1290px]">
+        <h2 className="text-xl">Rules of the game</h2>
+        <p>test</p>
+      </div>
     </section>
   );
 }
