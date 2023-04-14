540b98320ad44a251af59beea12857768e0fe6fa
Anthony Gorski <38192276+GorskiAnthony@users.noreply.github.com>
2023-04-14 14:28:18 +0200

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,10 +3,8 @@ import DisplayHouses from "@components/DisplayHouses";
 export default function Home() {
   return (
     <section>
-
       <h2 className="text-xl">Home</h2>
       <DisplayHouses />
-
     </section>
   );
 }
