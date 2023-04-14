0b0cb4054126f18cae8696442291a587c0678cfd
Anthony Gorski <38192276+GorskiAnthony@users.noreply.github.com>
2023-04-14 11:15:54 +0200

    Update SearchBar.jsx

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,12 +1,14 @@
-
-
 function SearchBar() {
   return (
-<div className= 'flex items-center relative'>
-      <input type="search" placeholder="Search.." className= "outline-none py-2 pl-9 pr-4 rounded-md w-80 bg-dark"></input>
-      <img src= "./public/icon/Glass.svg" className= "pl-2 absolute "/>
+    <div className="flex items-center relative">
+      <input
+        type="search"
+        placeholder="Search.."
+        className="outline-none py-2 pl-9 pr-4 rounded-md w-80 bg-dark"
+      />
+      <img src="./public/icon/Glass.svg" className="pl-2 absolute " />
     </div>
-)};
+  );
+}
 
 export default SearchBar;
-
