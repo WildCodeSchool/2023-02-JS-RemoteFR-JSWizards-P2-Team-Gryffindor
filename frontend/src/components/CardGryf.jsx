c1bff03a5183a3e54c58d0327d4324d073170117
Jeremy <[arkantik.pro@gmail.com](mailto:arkantik.pro@gmail.com)>
2023-04-19 11:02:57 +0200

    Added proptypes to card

--- frontend/src/components/CardGryf.jsx
+++ frontend/src/components/CardGryf.jsx
@@ -1,22 +1,58 @@
-export default function CardGryf() {
+import PropTypes from "prop-types";
+
+function Card({ name, image, house }) {
+  let backgroundSrc;
+  let logoSrc;
+
+  switch (house) {
+    case "Gryffindor":
+      backgroundSrc = "./public/image/CardGryf.png";
+      logoSrc = "./public/icon/gryf-logo.svg";
+      break;
+    case "Hufflepuff":
+      backgroundSrc = "./public/image/CardHuff.png";
+      logoSrc = "./public/icon/huff-logo.svg";
+      break;
+    case "Ravenclaw":
+      backgroundSrc = "./public/image/CardRav.png";
+      logoSrc = "./public/icon/rav-logo.svg";
+      break;
+    case "Slytherin":
+      backgroundSrc = "./public/image/CardSly.png";
+      logoSrc = "./public/icon/sly-logo.svg";
+      break;
+    default:
+      backgroundSrc = "./public/image/CardDefault.png";
+      logoSrc = "./public/icon/hogwarts-logo.svg";
+      break;
+  }
+
   return (
     <div className="relative rounded-xl">
       <img
         className="relative object-cover object-center w-[184px] h-[265px] rounded-xl shadow-sm"
-        src="./public/image/CardGryf.png"
-        alt="Gryffindor Card background"
+        src={backgroundSrc}
+        alt={`${house} Card background`}
       />
       <img
         className="absolute top-3 left-3 object-cover w-[160px] h-[210px] rounded-t-md"
-        src="./public/image/Harry.jpg"
-        alt="Harry potter"
+        src={image}
+        alt={name}
       />
       <img
         className="absolute bottom-2 right-1 w-10 h-10"
-        src="./public/icon/gryf-logo.svg"
-        alt="Gryffindor logo"
+        src={logoSrc}
+        alt={`${house} logo`}
       />
-      <p className="absolute bottom-3 left-3">Harry Potter</p>
+      <p className="absolute bottom-3 left-3">{name}</p>
     </div>
   );
 }
+
+Card.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  house: PropTypes.string.isRequired,
+};
+
+export default Card;
