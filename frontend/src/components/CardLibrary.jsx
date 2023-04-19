585d5381ecf7dd7172df1f39f4cbb8d1a740d496
Jeremy <[arkantik.pro@gmail.com](mailto:arkantik.pro@gmail.com)>
2023-04-19 11:18:22 +0200

    Added proptypes to CardLibrary

--- frontend/src/components/CardLibrary.jsx
+++ frontend/src/components/CardLibrary.jsx
@@ -1,13 +1,31 @@
-// import CardGryf from "./CardGryf";
+import PropTypes from "prop-types";
+import CardGryf from "./CardGryf";
 
-// export default function CardLibrary({ cards }) {
-//   return (
-//     <div className="gallery">
-//       {cards.map((card) => (
-//         <CardGryf key={card.id} {...card} />
-//       ))}
-//       <p>test</p>
-//       <p>test2</p>
-//     </div>
-//   );
-// }
+function CardLibrary({ cards }) {
+  return (
+    <div className="gallery">
+      {cards.map((card) => (
+        <CardGryf
+          key={card.id}
+          name={card.name}
+          image={card.image}
+          house={card.house}
+        />
+      ))}
+      <p>test</p>
+      <p>test2</p>
+    </div>
+  );
+}
+
+CardLibrary.propTypes = {
+  cards: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      house: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default CardLibrary;
