64130f812f22a8d3fef95e6a9f855ae06001c9c0
Thibaut Resse <thibaut.resse@outlook.fr>
2023-04-20 09:49:05 +0200

    cleaned comments

--- frontend/src/components/GameHistoryReact.jsx
+++ frontend/src/components/GameHistoryReact.jsx
@@ -27,29 +27,3 @@ export default function GameHistoryReact() {
     </div>
   );
 }
-
-/*
-          <tbody>
-            <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-              <td className="px-6 py-4">Silver</td>
-              <td className="px-6 py-4">Silver</td>
-              <td className="px-6 py-4">Laptop</td>
-              <td className="px-6 py-4">$2999</td>
-              <td className="px-6 py-4">test</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-
-      
-
-
-/*<thead>
-        <tr className='grid grid-rows-5 grid-cols-5 justify-center gap-x-4'>
-          <th>Your house</th>
-          <th>Your character</th>
-          <th>Enemy's house</th>
-          <th>Enemy's character</th>
-          <th>Result</th>
-        </tr>
-      </thead> */
