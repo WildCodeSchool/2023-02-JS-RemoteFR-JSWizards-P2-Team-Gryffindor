84bbef7c0c225e74beeb06d217c6afcef00f3417
Victor Fabier <[MY\_NAME@example.com](mailto:fabier.victor.1720@gmail.com)>
2023-04-20 17:39:51 +0200

    Correction d'orthographe

--- frontend/src/pages/Play.jsx
+++ frontend/src/pages/Play.jsx
@@ -3,7 +3,7 @@ import HousesCards from "../components/HousesCards";
 export default function Play() {
   return (
     <section className="flex flex-col items-center space-y-8 ">
-      <h2 className="text-xl">Chose your house</h2>
+      <h2 className="text-xl">Choose your house</h2>
       <p className="italic">
         Select the house you want to be in for your fight, your character will
         depend of the house you pick
