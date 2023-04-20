e63707e0674a969941a674ad2ff6e9499be4766d
Victor Fabier <[MY\_NAME@example.com](mailto:fabier.victor.1720@gmail.com)>
2023-04-20 17:35:27 +0200

    Ajout d'un paragrahe sur la page play

--- frontend/src/pages/Play.jsx
+++ frontend/src/pages/Play.jsx
@@ -4,6 +4,10 @@ export default function Play() {
   return (
     <section className="flex flex-col items-center space-y-8 ">
       <h2 className="text-xl">Chose your house</h2>
+      <p className="italic">
+        Select the house you want to be in for your fight, your character will
+        depend of the house you pick
+      </p>
       <HousesCards />
     </section>
   );
