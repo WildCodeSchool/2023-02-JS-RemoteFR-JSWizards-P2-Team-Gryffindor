6cafe877b88a410ba98629fc930d9c117da8d5a0
Jeremy <[arkantik.pro@gmail.com](mailto:arkantik.pro@gmail.com)>
2023-04-21 12:30:13 +0200

    Modified design of account page and imported empty Card component

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -1,13 +1,23 @@
+import Card from "../components/Card";
 import AccountBanner from "../components/AccountBanner";
 import GameHistoryReact from "../components/GameHistoryReact";
 
 export default function Account() {
   return (
-    <section className="space-y-4">
+    <section>
       <h2 className="text-xl">My account</h2>
-      <div className="bg-dark rounded">
-        <AccountBanner />
-        <GameHistoryReact />
+      <div className="bg-dark rounded p-4 space-y-8 justify-center">
+        <div className="flex justify-between">
+          <h2 className="text-lg">Games played per house</h2>
+          <h2 className="text-lg">Most played card</h2>
+        </div>
+        <div className="flex justify-around items-center">
+          <AccountBanner />
+          <Card />
+        </div>
+        <div>
+          <GameHistoryReact />
+        </div>
       </div>
     </section>
   );
