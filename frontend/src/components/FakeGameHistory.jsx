f2347e51065025b2eab940b5f72b9933d4c09b09
Thibaut Resse <thibaut.resse@outlook.fr>
2023-04-21 10:11:05 +0200

    game history now show characters with their proper houses

--- frontend/src/components/FakeGameHistory.jsx
+++ frontend/src/components/FakeGameHistory.jsx
@@ -9,23 +9,17 @@ export default function FakeGameHistory() {
     "Horace Slughorn",
   ];
   const fakeResult = ["won", "lost", "abandonned"];
-  
   const [games, setGames] = useState([]);
   useEffect(() => {
     const newGames = [];
 
     for (let i = 0; i < 4; i += 1) {
-      const random1 = Math.floor(Math.random() * fakeArrayHouse.length)
-      const random2 = Math.floor(Math.random() * fakeArrayHouse.length)
-
-      const house =
-        fakeArrayHouse[random1];
-      const character =
-        fakeArrayAi[random1];
-      const ennemyHouse =
-        fakeArrayHouse[random2];
-      const ennemyCharacter =
-        fakeArrayAi[random2];
+      const random1 = Math.floor(Math.random() * fakeArrayHouse.length);
+      const random2 = Math.floor(Math.random() * fakeArrayHouse.length);
+      const house = fakeArrayHouse[random1];
+      const character = fakeArrayAi[random1];
+      const ennemyHouse = fakeArrayHouse[random2];
+      const ennemyCharacter = fakeArrayAi[random2];
       const result = fakeResult[Math.floor(Math.random() * fakeResult.length)];
 
       newGames.push({
