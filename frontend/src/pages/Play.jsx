01a74cfb7171932dd793fa1e02861736485ed530
Jeremy <[arkantik.pro@gmail.com](mailto:arkantik.pro@gmail.com)>
2023-04-26 14:37:02 +0200

    Fixed height of play page

--- frontend/src/pages/Play.jsx
+++ frontend/src/pages/Play.jsx
@@ -2,7 +2,7 @@ import HousesCards from "../components/HousesCards";
 
 export default function Play() {
   return (
-    <section className="flex flex-col items-center space-y-8 ">
+    <section className="flex flex-col items-center space-y-8 h-[calc(100vh-125px)]">
       <h2 className="text-xl">Choose your house</h2>
       <p className="italic">
         Select the house you want to be in for your fight, your character will
