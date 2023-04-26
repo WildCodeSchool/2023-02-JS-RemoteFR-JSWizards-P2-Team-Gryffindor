cf1e5f612edeb9c25b2f6556bc9c3c90520402fd
Hafsa <hafsa.aitouarraou@outlook.fr>
2023-04-26 21:50:43 +0200

    récupérations du personnage choisi et du random gràace au local storage et apparition dans versus

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import "./App.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  //  </React.StrictMode>
 );
