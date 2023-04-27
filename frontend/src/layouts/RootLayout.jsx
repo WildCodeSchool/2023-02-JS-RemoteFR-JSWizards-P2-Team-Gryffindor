5b34f9747c9f04de28ddf75b5a5e07e5ed6d8fc3
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-04-27 17:15:57 +0200

    Fix of Navbar

--- frontend/src/layouts/RootLayout.jsx
+++ frontend/src/layouts/RootLayout.jsx
@@ -6,9 +6,7 @@ export default function RootLayout() {
   return (
     <div className="root-layout min-h-screen bg-darkmode text-primary">
       <header>
-        <nav className="sticky bg-dark flex justify-between items-center px-8 py-2.5">
-          <Navbar />
-        </nav>
+        <Navbar />
       </header>
 
       <main className="px-6 py-6 md:px-8 md:py-8">
