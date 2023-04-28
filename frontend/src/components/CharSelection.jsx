ffa88478f5626bc467849d0b4952b10a70633ac9
Jeremy Dohin <95821033+Arkantik@users.noreply.github.com>
2023-04-28 11:48:42 +0200

    Update CharSelection.jsx

--- frontend/src/components/CharSelection.jsx
+++ frontend/src/components/CharSelection.jsx
@@ -10,8 +10,8 @@ export default function CharSelection({
   info,
   pickedUpCard,
   setPickedUpCard,
-  description })
-
+  description 
+}) {
   const [filteredCards, setFilteredCards] = useState([]);
   const [cards, setCards] = useState([]);
   const [allCards, setAllCards] = useState([]);
