36eb3d92f7d2da475174d083a2501eead7a3a250
Hafsa <hafsa.aitouarraou@outlook.fr>
2023-05-02 17:37:12 +0200

    errors fixed

--- frontend/src/components/Versus.jsx
+++ frontend/src/components/Versus.jsx
@@ -34,15 +34,87 @@ function Versus() {
     fetchData();
   }, []);
 
+  // Stats
+  const [myCharacterHP, setMyCharacterHP] = useState(200);
+  const [myCharacterDP, setMyCharacterDP] = useState(
+    Math.floor(Math.random() * 30) + 100
+  );
+  const [myCharacterAP, setMyCharacterAP] = useState(
+    Math.floor(Math.random() * 30) + 100
+  );
+  const [enemyCharacterHP, setEnemyCharacterHP] = useState(200);
+  const [enemyCharacterDP, setEnemyCharacterDP] = useState(
+    Math.floor(Math.random() * 30) + 100
+  );
+  const [enemyCharacterAP, setEnemyCharacterAP] = useState(
+    Math.floor(Math.random() * 30) + 100
+  );
+
+  const myDamageSpell = () => {
+    const myDamage = Math.floor(Math.random() * 30) + 10; // dégâts aléatoires entre 10 et 30
+    setEnemyCharacterHP(enemyCharacterHP - myDamage);
+    if (enemyCharacterHP - myDamage <= 0) {
+      alert("You won!");
+      // message de victoire pour mon perso
+    }
+  };
+
+  const enemyDamageSpell = () => {
+    const enemyDamage = Math.floor(Math.random() * 30) + 10; // dégâts aléatoires entre 10 et 30
+    setMyCharacterHP(myCharacterHP - enemyDamage);
+    if (myCharacterHP - enemyDamage <= 0) {
+      alert(`${enemyCharacter.name} won!`);
+      // message de victoire pour l'ennemi
+    }
+  };
+
+  const myHandleAP = () => {
+    const points = Math.floor(Math.random() * 30) + 10;
+    setMyCharacterAP(myCharacterAP + points);
+  };
+
+  const myHandleDP = () => {
+    const points = Math.floor(Math.random() * 30) + 10;
+    setMyCharacterDP(myCharacterDP + points);
+  };
+
+  const myHandleHP = () => {
+    const points = Math.floor(Math.random() * 30) + 10;
+    setMyCharacterHP(myCharacterHP + points);
+  };
+
+  const enemyHandleAP = () => {
+    const points = Math.floor(Math.random() * 30) + 10;
+    setEnemyCharacterAP(enemyCharacterAP + points);
+  };
+
+  const enemyHandleDP = () => {
+    const points = Math.floor(Math.random() * 30) + 10;
+    setEnemyCharacterDP(enemyCharacterDP + points);
+  };
+
+  const enemyHandleHP = () => {
+    const points = Math.floor(Math.random() * 30) + 10;
+    setEnemyCharacterHP(enemyCharacterHP + points);
+  };
   return (
     <div className="flex flex-col justify-around min-h-[calc(100vh-210px)] bg-[url('./image/wood.jpg')] bg-cover rounded-xl w-full">
       <div className="flex justify-around items-center">
         <div className="justify-center items-center space-y-24">
           <div className="flex justify-around gap-8">
             <div className="flex flex-col justify-center items-center gap-8 potions">
-              <button type="button">AP Potion ⚔️</button>
-              <button type="button">DP Potion 🛡️</button>
-              <button type="button">HP Potion ❤️</button>
+              <button type="button" onClick={myHandleAP}>
+                AP Potion ⚔️
+                {myCharacterAP}
+              </button>
+              <button type="button" onClick={myHandleDP}>
+                DP Potion 🛡️
+                {myCharacterDP}{" "}
+              </button>
+              <button type="button" onClick={myHandleHP}>
+                HP Potion ❤️
+                {myCharacterHP}
+              </button>
             </div>
             {myCharacter && (
               <Card
@@ -50,13 +122,22 @@ function Versus() {
                 image={myCharacter.image}
                 house={myCharacter.house}
                 idwizard={myCharacter.id}
+                HP={myCharacterHP}
+                AP={myCharacterAP}
+                DP={myCharacterDP}
               />
             )}
           </div>
           <div className="flex flex-row gap-4 justify-center spells">
-            <button type="button">spells</button>
-            <button type="button">spells</button>
-            <button type="button">spells</button>
+            <button type="button" onClick={myDamageSpell}>
+              spells
+            </button>
+            <button type="button" onClick={myDamageSpell}>
+              spells
+            </button>
+            <button type="button" onClick={myDamageSpell}>
+              spells
+            </button>
           </div>
         </div>
         <div className="justify-center items-center space-y-24">
@@ -67,18 +148,36 @@ function Versus() {
                 image={enemyCharacter.image}
                 house={enemyCharacter.house}
                 idwizard={enemyCharacter.id}
+                HP={enemyCharacterHP}
+                AP={enemyCharacterAP}
+                DP={enemyCharacterDP}
               />
             )}
             <div className="flex flex-col justify-center items-center gap-8 potions">
-              <button type="button">AP Potion ⚔️</button>
-              <button type="button">DP Potion 🛡️</button>
-              <button type="button">HP Potion ❤️</button>
+              <button type="button" onClick={enemyHandleAP}>
+                AP Potion ⚔️
+                {enemyCharacterAP}
+              </button>
+              <button type="button" onClick={enemyHandleDP}>
+                DP Potion 🛡️
+                {enemyCharacterDP}
+              </button>
+              <button type="button" onClick={enemyHandleHP}>
+                HP Potion ❤️
+                {enemyCharacterHP}
+              </button>
             </div>
           </div>
           <div className="flex flex-row gap-4 justify-center spells">
-            <button type="button">spells</button>
-            <button type="button">spells</button>
-            <button type="button">spells</button>
+            <button type="button" onClick={enemyDamageSpell}>
+              spells
+            </button>
+            <button type="button" onClick={enemyDamageSpell}>
+              spells
+            </button>
+            <button type="button" onClick={enemyDamageSpell}>
+              spells
+            </button>
           </div>
         </div>
       </div>
