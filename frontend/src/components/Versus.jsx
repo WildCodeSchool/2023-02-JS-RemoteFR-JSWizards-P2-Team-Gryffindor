1761cf5702b28b25a354332b9fac9f9c00ad8e35
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-05-02 14:42:39 +0200

    Changed height of versus

--- frontend/src/components/Versus.jsx
+++ frontend/src/components/Versus.jsx
@@ -35,7 +35,7 @@ function Versus() {
   }, []);
 
   return (
-    <div className="flex flex-col justify-around min-h-[calc(100vh-125px)] bg-[url('./image/wood.jpg')] bg-cover rounded-2xl w-full">
+    <div className="flex flex-col justify-around min-h-[calc(100vh-210px)] bg-[url('./image/wood.jpg')] bg-cover rounded-xl w-full">
       <div className="flex justify-around items-center">
         <div className="justify-center items-center space-y-24">
           <div className="flex justify-around gap-4">
