9cc5e8b6a9263c91b035c52234701a47dcd38f30
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-05-03 16:36:12 +0200

    Fixed buttons in Login & Register

--- frontend/src/components/LoginCard.jsx
+++ frontend/src/components/LoginCard.jsx
@@ -102,7 +102,7 @@ export default function LoginCard() {
           </div>
           <button
             type="submit"
-            className="text-secondary py-2 px-5 w-full bg-primary hover:bg-secondary hover:text-primary rounded border border-gray-200 duration-300 ease-in-out"
+            className="text-secondary py-2 px-5 w-full bg-primary hover:bg-secondary hover:text-dark rounded border border-gray-200 duration-300 ease-in-out"
           >
             Sign in
           </button>
