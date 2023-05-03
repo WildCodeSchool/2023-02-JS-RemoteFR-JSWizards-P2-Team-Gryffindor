f35bdfaf6d0ecf79e9e2946f6c955a4b5d218136
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-05-03 21:28:32 +0200

    Fixed font style of h2 in Account page

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -13,7 +13,7 @@ export default function Account({ setAudioName }) {
   pickedUpCard = pickedUpCard ? JSON.parse(pickedUpCard) : null;
   return (
     <section className="space-y-4">
-      <h2 className="text-xl">My account</h2>
+      <h2 className="text-xl font-serif">My account</h2>
       <div className="bg-dark rounded p-4 space-y-8 justify-center">
         <div className="flex justify-around">
           <div className="space-y-8 text-center">
