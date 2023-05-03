6676faeb237a9d8d62f783166bd1a49010db83b6
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-05-03 14:44:43 +0200

    Modification of H1 in Hero

--- frontend/src/components/Home/Hero.jsx
+++ frontend/src/components/Home/Hero.jsx
@@ -1,11 +1,10 @@
 export default function Hero() {
   return (
     <div className="flex justify-center items-center">
-      <h1 className="text-white absolute text-center text-xl">
-        Welcome to,
-        <br />
-        Hogwarts Magic Cards
-      </h1>
+      <div className="text-white absolute text-center text-xl space-y-2">
+        <h1>Welcome to,</h1>
+        <h1 className="text-2xl font-serif">Hogwarts Magic Card$</h1>
+      </div>
       <img
         className="object-center object-cover max-h-96"
         src="./image/Hogwarts.png"
