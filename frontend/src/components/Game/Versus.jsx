574ee4b8149465865fe101afae8c15fa51cfdcb4
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-05-04 14:25:10 +0200

    Patch of bug where char with no house would have a random house applied

--- frontend/src/components/Game/Versus.jsx
+++ frontend/src/components/Game/Versus.jsx
@@ -7,14 +7,8 @@ import CharSpells from "./Spells";
 function Versus() {
   const [myCharacter, setMyCharacter] = useState(null);
   const [enemyCharacter, setEnemyCharacter] = useState(null);
-  const houseTab = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
 
   const MySwal = withReactContent(Swal);
-
-  function randomHouse() {
-    const randomIdx = Math.floor(Math.random() * 3);
-    return houseTab[randomIdx];
-  }
 
   const fetchData = async () => {
     // first step : fetch my character
@@ -37,7 +31,6 @@ function Versus() {
       const data = await response.json();
 
       const enemyData = data[0];
-      enemyData.house = enemyData.house ? enemyData.house : randomHouse();
 
       setEnemyCharacter(enemyData);
     } catch (error) {
